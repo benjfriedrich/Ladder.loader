5f7744045c451eb11c58a2a30ed4fad8ea34939a
Benjamin Friedrich <benjfriedrich@BFs-mac.local>
2020-01-08 17:50:03 -0600

    update helper text

--- ladderLoader.js
+++ ladderLoader.js
@@ -1,52 +1,21 @@
 
 /*/ 
 
-Ladder.loader() is a simple IMG loader for static-style sites that loads images one at time, but in tiers.
+Ladder.loader() is a simple <img> loader for static-style sites that loads images one at time, but in tiers.
+
 
 It requires you to properly format the relevant <img> tags in the raw HTML, and call Ladder.loader() in JavaScript.
 
-In HTML:
+	Here's a properly formated <img> tag:
 
-	1) Assign your <img> tags to tiers by class,
-	2) assign the actual link to a data slot,
-	3) & fill the src with a temporary image. 
-
-Here's an example:
-
-	<img class="first-tier" data-llsrc="/images/fullImage.jpg" src="/images/tempLoading.svg">
-
-In JavaScript:
-
-	1. Define a tier using a 2-element array with the tier class, and the url data-attribute (note: don't include 'data-'),
-
-		example: ['first-tier','llsrc']
-
-	2. pass as many of these arugments into Ladder.loader() as you want, in the order that you want them loaded,
-
-		example: Ladder.loader(['first-tier','llsrc'],['second-tier','llsrc'],['third-tier','llsrc'])
-
-	3. & then call Ladder.loader() at the appropriate time!
-
-		example: Window.onload = Ladder.loader(['first-tier','llsrc'])
-
-Pro-tip:
-
-	Remember you can pass functions that return strings as arguments. The most common use for this would be to
-	provide smaller versions of files to mobile devices.
-
-Here's an example:
-
-	In HTML:
-
-	<img class="first-tier" data-llsrcFull="/fullImage.jpg" data-llsrcMobile="/smallImage.jpg" src="/images/tempLoading.svg">
-
-	In JavaScript:
-
-	const isMobile = () => window.innerWidth <= 800 ? "llsrcMobile" : "llsrcFull";
-
-	Window.onload = Ladder.loader(['first-tier',isMobile()])
+<img class="first-tier" data-llsrc="/images/fullImage.jpg" src="/images/tempLoading.svg">
 
 
+	Here's a properly formated Ladder.loader() call:
+
+Window.onload = Ladder.loader(['first-tier','llsrc'],['second-tier','llsrc'],['third-tier','llsrc'])
+	
+More info at https://github.com/benjfriedrich/ladderLoader.js
 
 (by @benjfriedrich)
 
